e7526d616693563f7d7803283c357a41a32541ca
Jani Arvonen <jani.arvonen@gmail.com>
2017-06-04 21:25:48 +0300

    create customer -command missing name fails and is not sent to broker

---
+++ test/hours_api/commands/create_customer_test.clj
@@ -0,0 +1,33 @@
+(ns hours-api.commands.create-customer-test
+  (:require
+    [midje.sweet :refer :all]
+    [hours-api.commands.customer :as customer]
+    [hours-api.components.broker :as broker]
+    [hours-api.system :as system]))
+
+(def broker-component (:broker (system/test-system)))
+(def valid-data {
+    :customer-name "Firma Oy"
+    :customer-business-id "12345467-8"
+  })
+
+
+(def valid-cmd
+  (customer/new-create-customer-cmd valid-data))
+
+
+(def invalid-cmd-missing-name
+  (customer/new-create-customer-cmd
+    (merge valid-data {:customer-name nil})))
+
+
+(fact "valid create customer -command is sent to broker"
+  (customer/handle broker-component valid-cmd) => anything
+    (provided
+      (broker/send-command broker-component valid-cmd) => anything))
+
+
+(fact "create customer -command missing customer name fails"
+  (customer/handle broker-component invalid-cmd-missing-name) => (throws Throwable)
+  (provided
+    (broker/send-command anything anything) => anything :times 0))
